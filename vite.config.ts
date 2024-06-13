c4547abd1cecd35d7c35dab2f35eba92880aae39
linkscope <linkscope@foxmail.com>
2024-06-13 22:18:28 +0800

    chore: checker plugin integrate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {resolve} from 'node:path'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), checker({
+        typescript: true,
+        eslint: {
+            lintCommand: 'eslint src/**/*.{ts,tsx}',
+            useFlatConfig: true,
+        },
+    }),],
+    resolve: {
+        alias: {
+            '@/': `${resolve(__dirname, 'src')}/`,
+        },
+    },
 })
