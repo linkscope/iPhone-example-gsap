4f5cf6e87902b0eb963bc2cf795f475329ed8f08
linkscope <linkscope@foxmail.com>
2024-06-13 22:10:29 +0800

    chore: eslint config

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import antfu from '@antfu/eslint-config'
+import tailwind from 'eslint-plugin-tailwindcss'
+
+export default antfu({
+    typescript: true,
+    react: true
+}).append(...tailwind.configs['flat/recommended'])
