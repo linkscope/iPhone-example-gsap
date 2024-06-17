7c5c343a7f5b2f3881f0bf2a833c2af50e464654
linkscope <linkscope@foxmail.com>
2024-06-18 03:34:57 +0800

    chore: highlights ui complete

--- src/components/high-lights.tsx
+++ src/components/high-lights.tsx
@@ -1,3 +1,44 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { rightImg, watchImg } from '@/utils'
+
 export default function HighLights() {
-  return <div>Highlights</div>
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+      delay: 2,
+    })
+
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      delay: 2,
+      duration: 1,
+      stagger: 0.25,
+    })
+  }, [])
+
+  return (
+    <section id="highlights" className="common-padding h-full w-screen overflow-hidden bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 flex w-full items-end justify-between">
+          <h1 id="title" className="section-heading">先刷重点。</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <a
+              href="https://www.apple.com.cn/105/media/cn/iphone-15-pro/2023/2f337511-a940-4b57-b89c-1512b7507777/films/product/iphone-15-pro-product-tpl-cn-2023_16x9.m3u8"
+              className="link"
+            >
+              观看影片
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </a>
+            <a href="https://www.apple.com.cn/apple-events/" className="link">
+              观看特别活动
+              <img src={rightImg} alt="right" className="ml-2" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
